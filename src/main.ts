5c438aee9581d8c70e102a94567ce0a363e25b80
Momen Sherif <momensherif.2019@gmail.com>
2022-01-31 17:32:21 +0200

    add downloadable screen recorder

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
+// JUST TRYING
+// check https://github.com/wmik/use-media-recorder
+
 const video = document.getElementById('video') as HTMLVideoElement;
 const videoToggleBtn = document.getElementById(
   'video-toggle',
 ) as HTMLButtonElement;
 
 const constraints: MediaStreamConstraints = {
-  audio: {
-    echoCancellation: true,
-    noiseSuppression: true,
-    sampleRate: 44100, // 44.1kHZ
-  },
+  audio: false,
   video: true,
   // video: { width: 1280, height: 720 },
 };
@@ -45,10 +44,56 @@ const stopSharingBtn = document.getElementById(
 
 let streaming = false;
 
-startSharingBtn.addEventListener('click', e => {
+startSharingBtn.addEventListener('click', async e => {
   if (streaming) return;
-  startCapture();
+  const stream = await startCapture();
+  if (!stream) return;
   streaming = true;
+
+  let recordedChunks: Blob[] = [];
+
+  const options = {
+    mimeType: 'video/webm',
+  };
+  const mediaRecorder = new MediaRecorder(stream!, options);
+
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.start();
+
+  function handleDataAvailable(event: BlobEvent) {
+    console.log('data-available', event.data);
+    if (event.data.size > 0) {
+      recordedChunks.push(event.data);
+    }
+  }
+
+  mediaRecorder.onstop = function () {
+    console.log('stop');
+    download(recordedChunks);
+    recordedChunks = [];
+  };
+
+  // const recorder = new MediaRecorder(stream, {
+  //   mimeType: 'video/webm; codecs=vp9',
+  // });
+  // const data: Blob[] = [];
+
+  // recorder.ondataavailable = e => data.push(e.data);
+
+  // recorder.onerror = e => console.error(e.error.name);
+
+  // recorder.onstop = e => {
+  //   const video = document.createElement('video');
+  //   video.controls = true;
+  //   video.width = 700;
+  //   video.src = URL.createObjectURL(new Blob(data));
+  //   document.body.appendChild(video);
+  // };
+
+  // recorder.start();
+  // setTimeout(() => {
+  //   recorder.stop();
+  // }, 5000);
 });
 
 stopSharingBtn.addEventListener('click', e => {
@@ -61,7 +106,17 @@ async function startCapture() {
 
   try {
     captureStream = await navigator.mediaDevices.getDisplayMedia(constraints);
+    const audioStream = await navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .catch(e => {
+        throw e;
+      });
+    const [audioTrack] = audioStream.getAudioTracks();
+    captureStream.addTrack(audioTrack);
     video.srcObject = captureStream;
+
+    // Listen for video track ended to capture stop sharing
+    captureStream.getVideoTracks()[0].onended = stopCapture;
   } catch (error) {
     console.error('Error', error);
   }
@@ -73,6 +128,18 @@ function stopCapture() {
   const tracks = (video.srcObject as MediaStream)?.getTracks();
   tracks?.forEach(track => track.stop());
   video.srcObject = null;
+}
+
+function download(chunks: Blob[]) {
+  const blob = new Blob(chunks, {
+    type: 'video/webm',
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'test.webm';
+  a.click();
+  URL.revokeObjectURL(url);
 }
 
 // app().catch(error => {
